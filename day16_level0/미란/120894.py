f3d1b4a95980c34d57680291e8eb41d486d68c9b
leeAnalyst <117931611+leeAnalyst@users.noreply.github.com>
2023-02-10 12:19:50 +0900

    feat: 영어가 싫어요

---
+++ day16_level0/미란/120894.py
@@ -0,0 +1,26 @@
+'''
+영어가 싫은 머쓱이는 영어로 표기되어있는 숫자를 수로 바꾸려고 합니다. 문자열 numbers가 매개변수로 주어질 때, numbers를 정수로 바꿔 return 하도록 solution 함수를 완성해 주세요.
+'''
+
+def solution(numbers):
+    k = ''
+    result = ''
+    dic = {
+        'zero':'0',
+        'one': '1',
+        'two': '2',
+        'three': '3',
+        'four': '4',
+        'five': '5',
+        'six': '6',
+        'seven': '7',
+        'eight': '8',
+        'nine': '9'
+    }
+    for c in numbers:
+        k += c
+        if k in dic:
+            result += dic[k]
+            k = ''
+    return int(result)
+        
