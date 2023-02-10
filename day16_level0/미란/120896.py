c16612f7e113d910f743929f945d572aa14bc4f1
leeAnalyst <117931611+leeAnalyst@users.noreply.github.com>
2023-02-10 12:18:05 +0900

    feat: 한 번만 등장한 문자

---
+++ day16_level0/미란/120896.py
@@ -0,0 +1,19 @@
+'''
+문자열 s가 매개변수로 주어집니다. s에서 한 번만 등장하는 문자를 사전 순으로 정렬한 문자열을 return 하도록 solution 함수를 완성해보세요. 한 번만 등장하는 문자가 없을 경우 빈 문자열을 return 합니다.
+'''
+
+def solution(s):
+    result = []
+    
+    dic = {}
+    for c in s:
+        if c in dic:
+            dic[c] += 1
+        else:
+            dic[c] = 1
+    
+    for k, v in dic.items():
+        if v == 1:
+            result.append(k)
+            
+    return ''.join(sorted(result))
